baa9e7281b441277d418958e99b8235f7b6cfc36
JClark <mlitvinkox96@gmail.com>
2022-02-03 18:30:50 -0500

    Starting Files

---
+++ InventorySystem/Source.cpp
@@ -0,0 +1,18 @@
+/*
+EXISTING PREPROCESSOR FILES
+*/
+
+#include <iostream>
+
+
+//CUSTOM HEADER FILE
+#include "Inventory.h"
+
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	system("PAUSE");
+	return 0;
+}
